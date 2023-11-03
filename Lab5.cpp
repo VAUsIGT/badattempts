e53b5c3073c41d2bb79a63d6d0e014d48cda2520
Vladimir <Vladopadus@mail.ru>
2023-11-03 09:23:56 +0200

    last lab5

--- Lab5.cpp
+++ Lab5.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 
 bool starts_with3(int n) {
 	short m = 1;
@@ -25,33 +25,35 @@ int main() {
 	int mas[k];
 	int n;
 	bool flag = false;
-	std::cout << "Ââåäèòå äëèíó ïîñëåäîâàòåëüíîñòè: ";
+	std::cout << "Введите длину последовательности: ";
 	std::cin >> n;
-	if ((n == 0) or (n > 19)) {
-		std::cout << "Íåïðàâèëüíî, ïîïðîáóé. åù¸. ðàç.";
+	if ((n < 1) or (n > 19)) {
+		std::cout << "Неправильно, попробуй. ещё. раз.";
 		return 1;
 	}
-	std::cout << "Ââåäèòå ÷èñëà" << std::endl;
+	std::cout << "Введите числа" << std::endl;
 	for (int i = 0; i < n; i++) {
 		std::cin >> mas[i];
 	}
 	for (int i = 0; i < n; i++) {
-		if (starts_with3(mas[i])) {
+		if (starts_with3(mas[i])) {//  убрать фукцию
 			flag = true;
 			break;
 		}
 	}
+
 	if (flag)
 		for (short a = 0; a < n; a++)
 			for (short b = 0; b < n; b++)
 				if (mas[b] > mas[a])
 					std::swap(mas[a], mas[b]);
+
 	for (int i = 0; i < n; i++) {
 		std::cout << mas[i]<<" ";
 	}
 	std::cout << std::endl;
 
-	//2 óñëîâèå
+	//2 условие
 	const short M = 9;
 	const short N = 9;
 	int matrix[M][N];
@@ -59,38 +61,61 @@ int main() {
 	short b;
 	short count_max = SHRT_MIN;
 	short count;
-	std::cout << "Ââåäèòå ðàçìåð ìàòðèöû(M*N): " << std::endl;
+	std::cout << "Введите размер матрицы(M*N): " << std::endl;
 	std::cin >> a >> b;
-	std::cout << "Ââåäèòå ÷èñëà ìàòðèöû: " << std::endl;
+	std::cout << "Введите числа матрицы: " << std::endl;
 	for (short i = 0; i < a; i++)
 		for (short j = 0; j < b; j++)
 			std::cin >> matrix[i][j];
+	//for (short j = 0; j < b; j++) {//
+	//	count = 0;
+	//	for (short i = 0; i < a; i++)
+	//		if (matrix[i][j] < 0)
+	//			count += 1;
+	//	if (count > count_max) count_max = count;
+	//}
+	//for (short j = 0; j < b; j++) {//
+	//	count = 0;
+	//	for (short i = 0; i < a; i++)
+	//		if (matrix[i][j] < 0)
+	//			count += 1;
+	//	if (count == count_max) {
+	//		for (short i = 0; i < a; i++)
+	//			matrix[i][j] = -1;
+	//	};
+	//}
+	////////////////////////////////////////////
+	count_max = 0;
+	short max_column_index = -1;
+
 	for (short j = 0; j < b; j++) {
-		count = 0;
-		for (short i = 0; i < a; i++)
-			if (matrix[i][j] < 0)
-				count += 1;
-		if (count > count_max) count_max = count;
+		short count = 0;
+		for (short i = 0; i < a; i++) {
+			if (matrix[i][j] < 0) {
+				count++;
+			}
+		}
+		if (count > count_max) {
+			count_max = count;
+			max_column_index = j;
+		}
 	}
-	for (short j = 0; j < b; j++) {
-		count = 0;
-		for (short i = 0; i < a; i++)
-			if (matrix[i][j] < 0)
-				count += 1;
-		if (count == count_max) {
-			for (short i = 0; i < a; i++)
-				matrix[i][j] = -1;
-		};
+
+	if (max_column_index != -1) {
+		for (short i = 0; i < a; i++) {
+			matrix[i][max_column_index] = -1;
+		}
 	}
-	std::cout << "Âàøà ìàòðèöà: " << std::endl;
+	//////////////////////////////////////////////
+	std::cout << "Ваша матрица: " << std::endl;
 	for (short i = 0; i < a; i++) {
 		for (short j = 0; j < b; j++)
 			std::cout << matrix[i][j] << ' ';
 		std::cout << std::endl;
 	}
 	
-	// äîï
-	std::cout << "äîï" << std::endl;
+	// доп
+	std::cout << "доп" << std::endl;
 	if (flag)
 		for (short a = 0; a < n; a++)
 			for (short b = 0; b < n; b++)
